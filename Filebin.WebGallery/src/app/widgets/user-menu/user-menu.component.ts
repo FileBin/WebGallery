16d34c7b809a98ca8428896230ad779ab4e70751
filebin <alex.mousse9@gmail.com>
2024-02-27 13:56:28 +0300

    basic login improved

--- Filebin.WebGallery/src/app/widgets/user-menu/user-menu.component.ts
+++ Filebin.WebGallery/src/app/widgets/user-menu/user-menu.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { AuthStateProviderService } from '../../services/auth-state-provider.service';
+import { UserInfoDto } from '../../services/user-api-proxy.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-user-menu',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './user-menu.component.html',
   styleUrl: './user-menu.component.scss'
 })
 export class UserMenuComponent {
+  @Input({required: true}) userInfo!: UserInfoDto;
+
   constructor(private authStateProvider: AuthStateProviderService) {}
+
   logout() {
     this.authStateProvider.removeTokens();
   }
